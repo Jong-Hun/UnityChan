4241e599a3261fd6aa8f84d38d98262e5a998952
dongwook <dbzeta@naver.com>
2018-08-15 14:07:45 +0900

    로딩, 로그인

--- Assets/Resources/Script/Loading.cs
+++ Assets/Resources/Script/Loading.cs
@@ -1,22 +1,44 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Loading : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		// 로딩 애니메이션 시작
-        // 데이터 로드
-        // 
-	}
+    public GameObject MainPanel = null;
+
+    public GameObject LogoImage = null;
+    public GameObject LicenseImage = null;
+    public GameObject LoadingImage = null;
+    private SpriteImage spriteImage = null;
+
+    private bool isLoading = false;
+    private AsyncOperation op = null;
+
+    // Use this for initialization
+    void Start () {
+
+        // 메인 패널
+        MainPanel.SetActive(false);
+        // 로고 화면
+        LogoImage.SetActive(true);
+        // 라이센스
+        LicenseImage.SetActive(false);
+        // 로딩 화면
+        LoadingImage.SetActive(false);
+
+        StartCoroutine(LoadScene());
+        StartCoroutine(LoadData());
+        StartCoroutine(PlayImage());
+
+    }
 	
 	// Update is called once per frame
 	void Update () {
 		
 	}
 
-    void NewGame()
+    public void NewGame()
     {
         // 이어하기에 있던 프리펩스 삭제;
         PlayerPrefs.DeleteAll();
@@ -27,20 +49,29 @@ public class Loading : MonoBehaviour {
         ResetPlayerQuest();
         ResetPlayerEquipItem();
         ResetPlayerInventoryItem();
+
+        // 이미 로드해놓은 씬으로 전환
+        op.allowSceneActivation = true;
     }
 
-    void LoadGame()
+    public void LoadGame()
     {
 
     }
 
-    void Continue()
+    public void Continue()
     {
+        if(PlayerPrefs.GetString("Map") == "None")
+        {
+            Debug.Log("이어하기 가능한 데이터가 없습니다.");
+            return;
+        }
 
+        Debug.Log(PlayerPrefs.GetString("Map"));
     }
 
     // 플레이어 데이터 리셋
-    void ResetPlayerStatus()
+    public void ResetPlayerStatus()
     {
         PlayerPrefs.SetInt("HP", 0);
         PlayerPrefs.SetInt("MP", 0);
@@ -51,7 +82,7 @@ public class Loading : MonoBehaviour {
     }
 
     // 플레이어 위치 리셋
-    void ResetPlayerLocation()
+    public void ResetPlayerLocation()
     {
         PlayerPrefs.SetString("Map","None");
         PlayerPrefs.SetInt("PosX", 0);
@@ -63,7 +94,7 @@ public class Loading : MonoBehaviour {
     }
 
     // 플레이어 퀘스트(시나리오 및 서브) 리셋
-    void ResetPlayerQuest()
+    public void ResetPlayerQuest()
     {
         PlayerPrefs.SetInt("MainQuest", 0);
         PlayerPrefs.SetInt("SubQuest1", 0);
@@ -72,24 +103,126 @@ public class Loading : MonoBehaviour {
     }
 
     // 플레이어 아이템(착용중, 소유) 리셋
-    void ResetPlayerEquipItem()
+    public void ResetPlayerEquipItem()
     {
         PlayerPrefs.SetInt("Weapon", 0);
-        PlayerPrefs.SetInt("Weapon_U", 0);
+        PlayerPrefs.SetInt("Weapon_C", 0);
         PlayerPrefs.SetInt("Clothing", 0);
-        PlayerPrefs.SetInt("Clothing_U", 0);
+        PlayerPrefs.SetInt("Clothing_C", 0);
         PlayerPrefs.SetInt("Shoes", 0);
-        PlayerPrefs.SetInt("Shoes_U", 0);
+        PlayerPrefs.SetInt("Shoes_C", 0);
     }
 
     // 플레이어 아이템(착용중, 소유) 리셋
-    void ResetPlayerInventoryItem()
+    public void ResetPlayerInventoryItem()
     {
         for(int i = 1; i <= 50; i++)
         {
             PlayerPrefs.SetInt("Item" + i.ToString(), 0);
-            PlayerPrefs.SetInt("Item_U" + i.ToString(), 0);
+            PlayerPrefs.SetInt("Item_C" + i.ToString(), 0);
         }
     }
+
+    
+    IEnumerator LoadScene()
+    {
+        op = SceneManager.LoadSceneAsync("GameScene");
+        op.allowSceneActivation = false;
+
+        float timer = 0.0f;
+        while (!op.isDone)
+        {
+            yield return null;
+
+            timer += Time.deltaTime;
+
+            if (op.progress >= 0.9f)
+            {
+                isLoading = true;
+                break;
+            }
+        }
+    }
+
+    IEnumerator LoadData()
+    {
+
+        if (PlayerPrefs.GetString("Map") == "" || PlayerPrefs.HasKey("Map") == false)
+        {
+            ResetPlayerStatus();
+            ResetPlayerLocation();
+            ResetPlayerQuest();
+            ResetPlayerEquipItem();
+            ResetPlayerInventoryItem();
+        }
+
+        // 로딩 애니메이션 시작
+        // 데이터 로드
+        // 
+
+        yield return null;
+    }
+
+    IEnumerator PlayImage()
+    {
+        float timer = 0.0f;
+        while (true)
+        {
+            if (LogoImage.activeSelf)
+            {
+                timer += Time.deltaTime;
+
+                if (timer >= 3.0f)
+                {
+                    LogoImage.SetActive(false);
+                    LicenseImage.SetActive(true);
+                    timer = 0.0f;
+                }
+
+                yield return null;
+            }
+
+            if (LicenseImage.activeSelf)
+            {
+                timer += Time.deltaTime;
+
+                if (timer >= 3.0f)
+                {
+                    LicenseImage.SetActive(false);
+                    LoadingImage.SetActive(true);
+                    spriteImage = LoadingImage.transform.Find("Image").GetComponent<SpriteImage>();
+                    spriteImage.AddImages("Image/Loading");
+                    spriteImage.speed = 0.04f;
+                    spriteImage.isLoop = false;
+                    spriteImage.Play();
+                    timer = 0.0f;
+                }
+                yield return null;
+            }
+
+            if (LoadingImage.activeSelf)
+            {
+                timer += Time.deltaTime;
+
+                if (timer > spriteImage.GetTotalPlayTime() + 1)
+                {
+                    if(isLoading == true)
+                    {
+                        LoadingImage.SetActive(false);
+                        MainPanel.SetActive(true);
+                        this.transform.parent.gameObject.SetActive(false);
+                        timer = 0.0f;
+                        break;
+                    }
+                }
+
+                yield return null;
+            }
+        }
+        yield return null;
+    }
+
+    
+
 }
 
--- Assets/Resources/Script/SpriteImage.cs
+++ Assets/Resources/Script/SpriteImage.cs
@@ -7,40 +7,88 @@ public class SpriteImage : MonoBehaviour {
     
     public Sprite[] images = null;
     public bool isLoop = true;
-    public int speed = 0;
+    public float speed = 0.0f;
 
+    private int curImageCount = 0;
+    private bool isPlay = false;
 
     // Use this for initialization
     void Start () {
+        if (!isPlay)
+            Play();
+    }
+
+    public void Play()
+    {
         StartCoroutine(ChangeImage());
     }
 
     IEnumerator ChangeImage()
     {
-        int count = 0;
+        float timer = 0.0f;
         int maxImageCount = images.Length;
         int imageCount = 0;
+        isPlay = true;
 
-        while (isLoop)
+        if (speed == 0)
+            yield break;
+
+        while (true)
         {
             yield return null;
 
-            count++;
-            
-            if (count >= 100 / speed)
+            timer += Time.deltaTime;
+
+            if (timer > speed)
             {
-                count = 0;
+                timer = 0.0f;
                 this.gameObject.GetComponent<Image>().sprite = images[imageCount];
 
                 if (imageCount < maxImageCount - 1)
                     imageCount++;
                 else
+                {
+                    if (!isLoop)
+                        yield break;
+
                     imageCount = 0;
+                }
             }
+
+            curImageCount = imageCount;
         }
         
         yield return null;
     }
 
+    public void AddImages(string folderPath)
+    {
+        images = Resources.LoadAll<Sprite>(folderPath);
+    }
 
+    public void AddImage(string folderPath, string imageName)
+    {
+        Sprite image = Resources.Load<Sprite>(folderPath + "/" + imageName);
+        images[images.Length] = image;
+    }
+
+    public bool IsStop()
+    {
+        int maxImageCount = images.Length;
+        
+        if(isLoop == false)
+        {
+            if (curImageCount == maxImageCount - 1)
+                return true;
+        }
+
+        return false;
+    }
+
+    public float GetTotalPlayTime()
+    {
+        int maxImageCount = images.Length;
+
+        return speed * (maxImageCount);
+    }
 }
